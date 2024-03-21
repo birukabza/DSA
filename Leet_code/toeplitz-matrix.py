1d05f63efa5310372671e931044dbf3efeb970df
Biruk Abza <81579047+birukabza@users.noreply.github.com>
2024-03-22 01:21:37 +0300

    Create toeplitz-matrix.py

---
+++ Leet_code/toeplitz-matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: list[list[int]]) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
+
+        for i in range(row-1):
+            for j in range(col-1): 
+                if matrix[i][j]!=matrix[i+1][j+1]:
+                    return False
+        return True
+       
+
+        
