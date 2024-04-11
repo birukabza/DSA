42ec7a17a14a6f2c65897f46994ccb7bcd1f2ba3
Biruk Abza <81579047+birukabza@users.noreply.github.com>
2024-04-11 16:31:56 +0300

    Create find-all-anagrams-in-a-string.py

---
+++ Leet_code/find-all-anagrams-in-a-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        targetCount = Counter(p)
+        windowCount = Counter(s[:len(p)-1])
+
+        for i in range(len(p)-1, len(s)):
+            windowCount[s[i]] = windowCount.get(s[i], 0) + 1
+
+            if windowCount == targetCount:
+                res.append(i - len(p)+1)
+            
+            windowCount[s[i-len(p)+1]] -= 1
+
+            if windowCount[s[i-len(p)+1]] == 0:
+                del windowCount[s[i-len(p)+1]]
+               
+        return res
+        
