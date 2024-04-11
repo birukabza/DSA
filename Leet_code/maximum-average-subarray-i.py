2cf0264437e6af45387c2387031ce0bf5e3d1757
Biruk Abza <81579047+birukabza@users.noreply.github.com>
2024-04-11 14:48:20 +0300

    Create maximum-average-subarray-i.py

---
+++ Leet_code/maximum-average-subarray-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxAverage(self, nums: list[int], k: int) -> float:
+        s = 0 
+        for i in range(k):
+            s += nums[i]
+        max_avg = s/k
+        for i in range(k, len(nums)):
+            s += nums[i]
+            s -= nums[i-k]
+            avg = s/k
+            max_avg = max(max_avg, avg)
+        return max_avg
