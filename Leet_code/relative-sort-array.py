64cb38269636acb70393c786f416559480a93c43
Biruk Abza <81579047+birukabza@users.noreply.github.com>
2024-04-24 01:28:46 +0300

    Create relative-sort-array.py

---
+++ Leet_code/relative-sort-array.py
@@ -0,0 +1,19 @@
+from typing import List 
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr2_set = set(arr2)
+        res= []
+        for n in arr2:
+            count_n = arr1.count(n)
+            for _ in range(count_n):
+                res.append(n)
+        left=[]
+        for num in arr1:
+            if num not in arr2_set:
+                left.append(num)
+
+        left.sort()
+        res.extend(left)
+        return res
+
+        
