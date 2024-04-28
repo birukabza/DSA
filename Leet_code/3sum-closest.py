1cb16339d972c5dc835260bec5b8255ae11f9257
Biruk Abza <81579047+birukabza@users.noreply.github.com>
2024-04-28 19:48:01 +0300

    Update 3sum-closest.py

---
+++ Leet_code/3sum-closest.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        diff: int = None #for tracking the d/ce b/n the target and the current sum
+        closestSum = nums[0] + nums[1] + nums[-1]
+        n = len(nums)
+        nums.sort()
+
+        for i in range(n-2):
+            
+            j = i+1
+            k = n-1
+
+            while j < k:
+                currentSum = nums[i]+ nums[j]+nums[k]
+                diff = abs(target-currentSum)
+
+                if diff < abs(target-closestSum):
+                    closestSum = currentSum
+                
+                if currentSum > target:
+                    k-=1
+                else:
+                    j+=1
+            
+        return closestSum
+
+
+        
