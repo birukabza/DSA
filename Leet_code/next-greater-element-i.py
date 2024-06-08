ab96fd0cb5bfa50be329ec2178568be40605f796
Biruk Abza <81579047+birukabza@users.noreply.github.com>
2024-06-08 12:54:30 +0300

    Create next-greater-element-i.py

---
+++ Leet_code/next-greater-element-i.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        res = defaultdict(lambda: -1)
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                res[stack[-1]] = num
+                stack.pop()
+
+            stack.append(num)
+        
+        return [res[num] for num in nums1]
